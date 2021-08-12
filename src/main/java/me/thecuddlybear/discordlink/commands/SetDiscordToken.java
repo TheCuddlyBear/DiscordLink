2474af03a7743e9c2f9dbaa8b15217cf755126b6
TheCuddlyBear <basvthazeveld@outlook.com>
2021-08-12 13:16:22 +0200

    Created discord token command

---
+++ src/main/java/me/thecuddlybear/discordlink/commands/SetDiscordToken.java
@@ -0,0 +1,21 @@
+package me.thecuddlybear.discordlink.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SetDiscordToken implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(player.hasPermission("discordlink.changetoken")){
+
+            }
+        }
+        return false;
+    }
+}
